131231f63738827eae6b8718f73d4b1555c16491
mocha_show <fuka.sho0224@gmail.com>
2025-02-02 22:41:26 +0900

    wip

---
+++ Test/Assets/Script/Enemy.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.GraphicsBuffer;
+
+public class Enemy : MonoBehaviour
+{
+    public int HP;
+    [SerializeField] private float Speed;
+    [SerializeField] private TextMeshProUGUI DamageText;
+    Player player => Player.Instance;
+    private bool isfirst = true;
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private float BulletRate;
+
+    private void Start()
+    {
+        DamageText.enabled = true;
+        Invoke("Attack", BulletRate);
+    }
+    private void Attack()
+    {
+        GameObject bullets = Instantiate(Bullet, transform.position, Quaternion.identity);
+        EnemyBullet bullet = Bullet.GetComponent<EnemyBullet>();
+        bullet.SetPower(transform.position);
+        Invoke("Attack", BulletRate);
+    }
+    private void Update()
+    {
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            if (isfirst)
+                isfirst = false;
+            else
+            {
+                Vector3 pos = transform.position;
+                if (pos.x < 0)
+                    transform.position = new Vector3(8.5f, pos.y, pos.z);
+                else
+                    transform.position = new Vector3(-8.5f, pos.y, pos.z);
+            }
+          
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Attack")
+        {
+            Debug.Log("“–‚½‚Á‚½");
+            HP--;
+            DamageText.enabled = true;
+            DamageText.text = "1";
+        }
+            
+        else if(collision.gameObject.tag =="Bullet")
+        {
+            Debug.Log("“–‚½‚Á‚½");
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            HP -= bullet.Damage;
+            DamageText.enabled = true;
+            DamageText.text = bullet.Damage.ToString();
+        }
+
+        if (HP < 0)
+            Destroy(this.gameObject);
+    }
+}
