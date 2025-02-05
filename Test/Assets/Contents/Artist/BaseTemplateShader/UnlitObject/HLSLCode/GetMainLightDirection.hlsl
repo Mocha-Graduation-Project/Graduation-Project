20bd79d1de5269aed8fa411cfdfad95b283cdf7c
Nyasa0405 <105965435+Nyasa0405@users.noreply.github.com>
2025-02-05 21:20:46 +0900

    光方向参照の法線マッピングの試作

---
+++ Test/Assets/Contents/Artist/BaseTemplateShader/UnlitObject/HLSLCode/GetMainLightDirection.hlsl
@@ -0,0 +1,21 @@
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+
+void GetMainLightDirection_float(out float3 LightColor,out float3 Direction, out float LightStrength)
+{
+#ifdef SHADERGRAPH_PREVIEW
+
+Direction = half3(0.5,0.5,0);
+LightColor =1;
+LightStrength = 1.0f;
+
+#else 
+    Light light = GetMainLight(0);
+    Direction = light.direction;
+    LightColor = light.color;
+    LightStrength = length(light.color);
+#endif
+}
+
+#endif
