d98f477a1d2db67d157b5f4113d2306a1d6c0f47
ebifry22 <g2227011@st.t-kougei.ac.jp>
2025-04-29 23:32:44 +0900

    シールドの追加

---
+++ Test/Assets/Scripts/Shield.cs
@@ -0,0 +1,60 @@
+using Scripts;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    enum ShieldPosition
+    {
+        left = 0,
+        right = 1,
+        up = 2,
+        down = 3,
+    }
+
+    [SerializeField] private ShieldPosition shieldPosition;
+    string playerBulletTag = "Bullet";
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(playerBulletTag) == true)
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            float speed = 0f;
+            //Debug.Log("ball:" + bullet.GetPower());
+            switch (shieldPosition)
+            {
+                case ShieldPosition.left:
+                    speed = bullet.GetPower().x;
+                    break;
+                case ShieldPosition.right:
+                    speed = bullet.GetPower().x * (-1);
+                    break;
+                case ShieldPosition.up:
+                    speed = bullet.GetPower().y * (-1);
+                    break;
+                case ShieldPosition.down:
+                    speed = bullet.GetPower().y;
+                    break;
+                default:
+                    break;
+            }
+
+            if (speed > 0)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
