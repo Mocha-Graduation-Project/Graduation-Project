d461100a0b22f6501076e292c84ec7e1ec78041a
ebifry22 <g2227011@st.t-kougei.ac.jp>
2025-05-14 14:43:45 +0900

    打ち返せる弾を撃つ敵を追加

--- Test/Assets/Scripts/Enemy.cs
+++ Test/Assets/Scripts/Enemy.cs
@@ -12,6 +12,12 @@ namespace Scripts
 
     public class Enemy : MonoBehaviour
     {
+        enum BulletType
+        {
+            enemyBullet,
+            reflectionBullet,
+        }
+        [SerializeField] private BulletType bulletType;
         public int HP;
         [SerializeField] private TextMeshProUGUI DamageText;
         Player player => Player.Instance;
@@ -35,8 +41,17 @@ namespace Scripts
         private void Attack()
         {
             GameObject bullets = Instantiate(Bullet, transform.position, Quaternion.identity);
-            EnemyBullet bullet = bullets.GetComponent<EnemyBullet>();
-            bullet.SetPower(transform.position);
+            switch (bulletType)
+            {
+                case BulletType.enemyBullet:
+                    EnemyBullet bullet = bullets.GetComponent<EnemyBullet>();
+                    bullet.SetPower(transform.position);
+                    break;
+                case BulletType.reflectionBullet:
+                    Bullet reflectionBullet = bullets.GetComponent<Bullet>();
+                    reflectionBullet.SetPowerEnemy(transform.position);
+                    break;
+            }
             audioSource.PlayOneShot(ShotSound);
             Invoke("Attack", BulletRate);
         }
