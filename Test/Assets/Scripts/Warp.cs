9eb34304cb8377dd57aa62ab7fef197364cb1a18
sora <4810sora01@gmail.com>
2025-06-08 19:59:17 +0900

    ワープの実装

---
+++ Test/Assets/Scripts/Warp.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Scripts
+{
+    public class Warp : MonoBehaviour
+    {
+        [SerializeField,JapaneseLabel("ワープ先")]private Transform targetWarp;
+        [SerializeField,JapaneseLabel("ワープさせるタグ"),Tag]private List<string> Tags = new List<string>() { "Player" };
+
+        private bool isWarping = false;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log($"触れた: {other.tag}");
+            // タグがリストに含まれていて、現在ワープ中でないなら処理する
+            if (!isWarping && Tags.Contains(other.tag))
+            {
+                Debug.Log($"タグ一致、ワープ開始: {other.name}");
+                StartCoroutine(WarpPoint(other));
+            }
+        }
+        private IEnumerator WarpPoint(Collider2D target)
+        {
+            isWarping = true;
+
+            Warp targetWarpPoint = targetWarp.GetComponent<Warp>();
+            if (targetWarpPoint != null)
+            {
+                targetWarpPoint.isWarping = true;
+            }
+
+            // 親（ルート）ごと移動
+            Transform rootTransform = target.transform.root;
+
+            // CharacterController対策
+            CharacterController controller = rootTransform.GetComponent<CharacterController>();
+            if (controller != null)
+            {
+                controller.enabled = false;
+                rootTransform.position = targetWarp.position;
+                controller.enabled = true;
+            }
+            else
+            {
+                rootTransform.position = targetWarp.position;
+            }
+
+            yield return new WaitForSeconds(0.2f);
+
+            isWarping = false;
+            if (targetWarpPoint != null)
+            {
+                targetWarpPoint.isWarping = false;
+            }
+        }
+    }
+}
