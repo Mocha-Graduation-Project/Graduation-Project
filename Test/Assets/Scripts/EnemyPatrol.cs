afec898afc5c01680ceb9c6c23f881f3dfc939f1
ebifry22 <g2227011@st.t-kougei.ac.jp>
2025-08-28 13:46:42 +0900

    EnemyPatrol修正

--- Test/Assets/Scripts/EnemyPatrol.cs
+++ Test/Assets/Scripts/EnemyPatrol.cs
@@ -6,13 +6,16 @@ using UnityEngine.Serialization;
 public class EnemyPatrol : MonoBehaviour
 {
     [SerializeField] PatrolEnemyData patrolEnemyData;
-    [SerializeField] float speed;
     private float leftMax, rightMax, upMax, downMax;
-    [SerializeField] Vector2 movement;
     [SerializeField] private bool moveable;
     private Collider loopAreaCollider;
-    private float enemySize = 0.5f;
+    private float enemySize = 1.0f;
     private Vector3 basePos;
+    float startTime;
+    private bool isMoveRightOrUp;
+    private float moveTime;
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 endPos;
     
     private void Awake()
     {
@@ -31,6 +34,7 @@ public class EnemyPatrol : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        startTime = Time.time;
         Bounds bounds = loopAreaCollider.bounds;
         basePos = transform.position;
         //Debug.Log("basePos:"+basePos);
@@ -49,18 +53,23 @@ public class EnemyPatrol : MonoBehaviour
         
         //Debug.Log("left:"+leftMax+"right:"+rightMax+"up:"+upMax+"down:"+downMax);
         
+        startPos = basePos;
+        
         switch (patrolEnemyData.State)
         {
             case PatrolEnemyData.EnemyState.vertical:
-                movement = Vector2.up;
+                endPos = new Vector3(basePos.x, upMax, basePos.z);
+                moveTime = patrolEnemyData.MoveVerticalTime;
                 break;
             case PatrolEnemyData.EnemyState.horizontal:
-                movement = Vector2.right;
+                endPos = new Vector3(rightMax, basePos.y, basePos.z);
+                moveTime = patrolEnemyData.MoveHorizontalTime;
                 break;
             case PatrolEnemyData.EnemyState.none:
                 break;
         }
-        
+
+        isMoveRightOrUp = true;
         moveable = true;
     }
 
@@ -69,82 +78,66 @@ public class EnemyPatrol : MonoBehaviour
     {
         if (moveable == true)
         {
-            transform.Translate(movement * speed * Time.deltaTime);
-
-            //端に着いたか調べる
-            CheckEdge();
+            Move(startPos, endPos, moveTime);
         }
     }
 
-    void CheckEdge()
+    void Move(Vector3 start, Vector3 end, float time)
     {
-        Vector3 pos = transform.position;
-        
+        float diff = Time.time - startTime;
+        if (diff > time)
+        {
+            NextMove();
+            return;
+        }
+        float rate = diff / time;
+        transform.position = Vector3.Lerp(start, end, rate);
+    }
+
+    void NextMove()
+    {
         switch (patrolEnemyData.State)
         {
             case PatrolEnemyData.EnemyState.vertical:
-                if (transform.position.y <= downMax)
+                startPos = transform.position;
+                moveTime = patrolEnemyData.MoveVerticalTime * 2;
+                if (isMoveRightOrUp == true)
                 {
-                    pos.y = downMax;
-                    moveable = false;
-                    movement = Vector2.up;
+                    endPos = new Vector3(startPos.x, downMax, startPos.z);
                 }
-                else if (transform.position.y >= upMax)
+                else
                 {
-                    pos.y = upMax;
-                    moveable = false;
-                    movement = Vector2.down;
+                    endPos = new Vector3(basePos.x, upMax, basePos.z);
                 }
                 break;
             case PatrolEnemyData.EnemyState.horizontal:
-                if (transform.position.x <= leftMax)
+                startPos = transform.position;
+                moveTime = patrolEnemyData.MoveHorizontalTime * 2;
+                if (isMoveRightOrUp == true)
                 {
-                    pos.x = leftMax;
-                    moveable = false;
-                    movement =Vector2.right;
+                    endPos = new Vector3(leftMax, basePos.y, basePos.z);
                 }
-                else if (transform.position.x >= rightMax)
+                else
                 {
-                    pos.x = rightMax;
-                    moveable = false;
-                    movement = Vector2.left;
+                    endPos = new Vector3(rightMax, basePos.y, basePos.z);
                 }
                 break;
-            default:
-                return;
+            case PatrolEnemyData.EnemyState.none:
+                break;
         }
-        transform.position = pos;
-
-        if (moveable == false)
-        {
-            Invoke("ChangeMoveable", patrolEnemyData.WaitTime);
-        }
+        moveable = false;
+        isMoveRightOrUp = !isMoveRightOrUp;
+        Invoke("ChangeMoveable", patrolEnemyData.WaitTime);
+    }
+    
+    void Initialization()
+    {
+        startTime = Time.time;
     }
 
     void ChangeMoveable()
     {
         moveable = !moveable;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        //地面、もしくは壁に当たった時に折り返す(壁の場合は未実装)
-        if(other.gameObject.CompareTag("Ground"))
-        {
-            moveable = false;
-            movement *= -1;
-            Invoke("ChangeMoveable", patrolEnemyData.WaitTime);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        //地面、もしくは壁に当たった時に折り返す(壁の場合は未実装)
-        if(other.gameObject.CompareTag("Ground"))
-        {
-            moveable = false;
-            movement *= -1;
-            Invoke("ChangeMoveable", patrolEnemyData.WaitTime);
-        }
+        Initialization();
     }
 }
