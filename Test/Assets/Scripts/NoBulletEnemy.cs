a289939a97612df9028fa6d664fdfe43b8c25546
sora <4810sora01@gmail.com>
2025-09-23 02:56:48 +0900

    撃破と盾直しました BGMはステージごとにMapManagerのAudioSourceに直接入れてください

--- Test/Assets/Scripts/NoBulletEnemy.cs
+++ Test/Assets/Scripts/NoBulletEnemy.cs
@@ -95,32 +95,5 @@ namespace Scripts
                 //Destroy(this.gameObject);
             }
         }
-
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            //if (collision.gameObject.tag == "Attack")
-            //{
-            //    Debug.Log("当たった");
-            //    HP--;
-            //    damageText.ShowDamage(1);
-            //    audioSource.PlayOneShot(DamageSound);
-            //}
-
-            if (collision.gameObject.tag == "Bullet")
-            {
-                Debug.Log("当たった");
-                Bullet bullet = collision.gameObject.GetComponent<Bullet>();
-                HP -= bullet.Damage;
-                damageText.ShowDamage(bullet.Damage);
-                audioSource.PlayOneShot(DamageSound);
-            }
-
-            if (HP <= 0)
-            {
-                audioSource.PlayOneShot(EnemyDestorySound);
-                enemySpawn.RemoveEnemy(this.gameObject);
-                //Destroy(this.gameObject);
-            }
-        }
     }
 }
--- Test/Assets/Scripts/Shield.cs
+++ Test/Assets/Scripts/Shield.cs
@@ -51,38 +51,6 @@ public class Shield : MonoBehaviour
 
             if (speed > 0)
             {
-                Destroy(collision.gameObject);
-            }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag(playerBulletTag) == true)
-        {
-            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
-            float speed = 0f;
-            //Debug.Log("ball:" + bullet.GetPower());
-            switch (shieldPosition)
-            {
-                case ShieldPosition.left:
-                    speed = bullet.GetPower().x;
-                    break;
-                case ShieldPosition.right:
-                    speed = bullet.GetPower().x * (-1);
-                    break;
-                case ShieldPosition.up:
-                    speed = bullet.GetPower().y * (-1);
-                    break;
-                case ShieldPosition.down:
-                    speed = bullet.GetPower().y;
-                    break;
-                default:
-                    break;
-            }
-
-            if (speed > 0)
-            {
                 audioSource.PlayOneShot(shieldSound);
                 Destroy(collision.gameObject);
             }
